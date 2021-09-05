93da459b41ef405678207518fc042fc8feb97dee
Ayush Rameja <67544555+RamejaAyush@users.noreply.github.com>
2021-09-05 19:08:51 +0530

    added react and removed unwanted files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "../styles/App.css";
+
+const App = () => {
+ const [name, setName] = useState("Legend");
+ return (
+  <>
+   <h1>Hi react</h1>
+  </>
+ );
+};
+
+export default App;
